e462461997038d0bb3f89a4893a633b7e311cee7
mwickramanayaka <manulwick@gmail.com>
2021-06-03 23:43:47 +0530

    SOP to identify log levels

--- src/main/java/demo/Log4jDemo.java
+++ src/main/java/demo/Log4jDemo.java
@@ -7,14 +7,22 @@ public class Log4jDemo {
 	
 	private static Logger logger = LogManager.getLogger(Log4jDemo.class);
 	
+
 	public static void main(String[] args) {
 
-		System.out.println("hello world \n");
-		
-		logger.trace("This is a trace message");
+		System.out.println("hello world 1 \n");
 		logger.info("This is info message");
+		
+		System.out.println("hello world 2 \n");
+		logger.trace("This is trace message");
+		
+		System.out.println("hello world 3 \n");
 		logger.error("This is error message");
+		
+		System.out.println("hello world 4 \n");
 		logger.warn("This is warn message");
+		
+		System.out.println("hello world 5 \n");
 		logger.fatal("This is fatal message");
 		
 		System.out.println("completed");
