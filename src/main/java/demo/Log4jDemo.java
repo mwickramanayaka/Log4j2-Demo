d0b9f82b7a3f20bf914ec2b072e8c90865d31546
mwickramanayaka <manulwick@gmail.com>
2021-06-07 23:33:57 +0530

    logger.error added

--- src/main/java/demo/Log4jDemo.java
+++ src/main/java/demo/Log4jDemo.java
@@ -7,7 +7,6 @@ public class Log4jDemo {
 	
 	private static Logger logger = LogManager.getLogger(Log4jDemo.class);
 	
-
 	public static void main(String[] args) {
 
 		System.out.println("hello world 1 \n");
